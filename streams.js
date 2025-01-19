51e7270c3919e78297a089be5ae345d8b6d12cc7
Abay Tesfaye <abaytesfaye02@gmail.com>
2025-01-19 10:35:20 +0300

    Use piping

--- streams.js
+++ streams.js
@@ -3,14 +3,17 @@ const fs = require('fs')
 // Reading file
 const readStream = fs.createReadStream('./docs/blog2.txt', { encoding: 'utf8' });
 const writeStream = fs.createWriteStream('./docs/blog3.txt');
-readStream.on('data', (chunk) => {
-    console.log('----NEW CHUNK----');
-    console.log(chunk);
+// readStream.on('data', (chunk) => {
+//     console.log('----NEW CHUNK----');
+//     console.log(chunk);
 
-    // Writing file
-    writeStream.write('\nNEW CHUNK\n');
-    writeStream.write(chunk);
-})
+//     // Writing file
+//     writeStream.write('\nNEW CHUNK\n');
+//     writeStream.write(chunk);
+// })
+
+// Piping
+readStream.pipe(writeStream);
 
 
 
