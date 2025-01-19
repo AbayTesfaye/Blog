99a11adfbb6a61d6356657d603b592f033a276a7
Abay Tesfaye <abaytesfaye02@gmail.com>
2025-01-19 10:28:14 +0300

    Read and Write using Stream

--- files.js
+++ files.js
@@ -1,50 +1,50 @@
 const fs = require('fs')
 // Reading file
-fs.readFile('./docs/blog.txt', (err, data) => {
-    if(err) {
-        console.log(err);
+// fs.readFile('./docs/blog.txt', (err, data) => {
+//     if(err) {
+//         console.log(err);
         
-    }
-    console.log(data.toString());
+//     }
+//     console.log(data.toString());
     
-})
+// })
 
 // Writing file
 
-fs.writeFile('./docs/blog1.txt',"New file gonna write",() => {
+fs.writeFile('./docs/blog2.txt',"New file gonna write",() => {
     console.log('file was written')
 })
 
 
-// Directories
-if(!fs.existsSync('./assets')) {
-fs.mkdir('./assets', (err)=> {
-  if(err) {
-    console.log(err);
-  }
-  console.log('Folder is created!');
+// // Directories
+// if(!fs.existsSync('./assets')) {
+// fs.mkdir('./assets', (err)=> {
+//   if(err) {
+//     console.log(err);
+//   }
+//   console.log('Folder is created!');
   
-})
-}
-else {
-    fs.rmdir('./assets',(err) => {
-        if(err) {
-            console.log(err);  
-        }
-        console.log('The folder is deleted');
+// })
+// }
+// else {
+//     fs.rmdir('./assets',(err) => {
+//         if(err) {
+//             console.log(err);  
+//         }
+//         console.log('The folder is deleted');
          
-    })
-}
+//     })
+// }
 
 
 // Delete file
 
-if(fs.existsSync('./docs/blog1.txt')) {
-    fs.unlink('./docs/blog1.txt', (err) => {
-        if(err) {
-            console.log(err); 
-        }
-        console.log('File is deleted!');
+// if(fs.existsSync('./docs/blog1.txt')) {
+//     fs.unlink('./docs/blog1.txt', (err) => {
+//         if(err) {
+//             console.log(err); 
+//         }
+//         console.log('File is deleted!');
         
-    })
-}
+//     })
+// }
