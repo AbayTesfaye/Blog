0d653c8c33596d514a9b3f65b4b5ec1ee7062da2
Abay Tesfaye <abaytesfaye02@gmail.com>
2025-01-19 11:47:56 +0300

    First request and response

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write('<head><link rel="stylesheet" href="#"></head>');
+    res.write('<p>Hello, Abay</p>');
+    res.write('<p>Hello again, Abay</p>');
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {
