dbb52e88311c224b936c6cee67442f3551934804
Abay Tesfaye <abaytesfaye02@gmail.com>
2025-01-22 14:51:08 +0300

    passing data into views

--- app.js
+++ app.js
@@ -11,21 +11,26 @@ app.listen(3000, () => {
 
 // Serve the index.ejs file
 app.get('/', (req, res) => {
-    res.render('index');
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      ];
+    res.render('index',{title: 'Home', blogs});
 });
 
 // Serve the about.ejs file
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about',{title: 'About'});
 });
 
 // Redirect to the about page
 app.get('/blogs/create', (req, res) => {
-    res.render('create'); // Assuming you want to render the 'about.ejs' file here
+    res.render('create',{title: 'Create'}); // Assuming you want to render the 'about.ejs' file here
 });
 
 // Handle 404 errors
 app.use((req, res) => {
-    res.status(404).render('404'); // Ensure the 404 file exists in your views folder
+    res.status(404).render('404',{title: '404'}); // Ensure the 404 file exists in your views folder
 });
 
